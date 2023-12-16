4f782d5ca709a97fe164eab2ff96774078814cee
kate☆ <143502858+kate-runion@users.noreply.github.com>
2023-12-16 02:52:28 +0000

    i hat ths blurgl

--- assignments/playlist.js
+++ assignments/playlist.js
@@ -1,13 +1,14 @@
-window.onload = init;
 function init() {
-var button = document.getElementById("addButton");
-button.onclick = handleButtonClick;
-}
-function handleButtonClick() {
+    var button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
+    loadPlaylist();
+    }
+    function handleButtonClick() {
     var textInput = document.getElementById("songTextInput");
     var songName = textInput.value;
     var li = document.createElement("li");
     li.innerHTML = songName;
-    var ul = document.getElementById("playlist");
+    var ul = document.getElementById("list");
     ul.appendChild(li);
-}
+    save(songName);
+    }
